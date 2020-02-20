d988ae39288e861fd38931b53575b9b139bcdce6
wolfram77 <wolfram77@gmail.com>
2020-02-21 00:59:45 +0530

    add assets

---
+++ assets/objdump.sh
@@ -0,0 +1,7 @@
+objdump --no-show-raw-insn \
+        -M intel           \
+        -Dj .text $(which ls) | 
+             sed -n '/<\.text>/, $ p'      | 
+             awk '{$1 = ""; print}'        |
+             sed '1d'                      |
+             awk '{print $1}' | sort | uniq -c
